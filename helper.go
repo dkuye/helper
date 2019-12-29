78311e8efdef571c9b58b233ba6373f55826cce1
Deji Kuye <dkuyebox@gmail.com>
2019-12-29 11:45:51 +0100

    add MySqlTimeStampToUnixTimeStamp

--- helper.go
+++ helper.go
@@ -21,6 +21,12 @@ func MySqlTimeStamp() time.Time {
 	return myTime
 }
 
+func MySqlTimeStampToUnixTimeStamp(mysqlTS string) int64  {
+	layout := "2006-01-02 15:04:05"
+	t, _ := time.Parse(layout, mysqlTS)
+	return t.Unix()
+}
+
 func FormatNumber(number int) string {
 	ac := accounting.Accounting{Symbol: "", Precision: 0}
 	return ac.FormatMoney(number)
